c50233f4578677f9d12d9cd4df76bc1b66baaa2f
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-05-11 10:22:37 +0800

    Create 1024 BT7086详情页磁链.js

---
+++ 1024 BT7086详情页磁链.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         1024 BT7086详情页磁链
+// @include      *://*.downsx.*/*
+// @version      0.1
+// @description  try to take over the world!
+// @author       93xo.ox39@gmail.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+document.querySelector('.dlboxbg').innerHTML="<a href=\'"+document.querySelectorAll(".uk-button")[1].href+" \'>"+document.querySelectorAll(".uk-button")[1].href+"</a>"
+    // Your code here...
+})();
