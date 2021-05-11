b82c73d7c1de28a0fdb834583b9681af58534144
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-05-11 10:38:20 +0800

    Create 2048 详情页直达.js

---
+++ 2048 详情页直达.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         2048 详情页直达
+// @include      *://download.bbcd.tw/*
+// @include      *://download.downsx.org/*
+// @version      0.1
+// @description  try to take over the world!
+// @author       93xo.ox39@gmail.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    'use strict';
+document.querySelector('.dlboxbg').innerHTML="<a href=\'"+document.querySelector(".uk-button").href+" \'>"+document.querySelector(".uk-button").href+"</a>"
+    // Your code here...
+})();
