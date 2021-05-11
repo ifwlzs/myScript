78141f8e7ec77217614b23648805332e3f492441
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-05-11 10:23:09 +0800

    Update A岛去广告.js

--- A岛去广告.js
+++ A岛去广告.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       93xo.ox39@gmail.com
 // @include        *://adnmb*.*/*
 // @grant        none
 // ==/UserScript==
