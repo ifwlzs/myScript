110f9b90ac9b02b0bbecd1626fa14087de3cb83d
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-05-11 10:13:52 +0800

    Create A岛去广告.js

---
+++ A岛去广告.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         A岛去广告
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include        *://adnmb*.*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    /*setTimeout(function(){
+    var x = document.querySelectorAll("");
+    for (var i = 0; i < x.length; i++) {
+        //console.log(x[i].parentNode.parentNode.parentNode)
+        x[i].parentNode.parentNode.parentNode.style.display="none"
+    }},3000
+   )*/
+     document.querySelector("#h-content > div.uk-container > div.h-threads-list > div > div.h-threads-item-replys > div:nth-child(1) > div.h-threads-item-reply-main > div.h-threads-info > a").parentNode.parentNode.parentNode.style.display='none'
+    // Your code here...
+})();
