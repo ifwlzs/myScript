cef19baa8b8221cb2bda86170f5390f689065222
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-11-16 16:11:02 +0800

    Update 98堂色花堂磁链直达.js

--- 98堂色花堂磁链直达.js
+++ 98堂色花堂磁链直达.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         98堂色花堂磁链直达
 // @include      *
-// @version      0.1
+// @version      0.2
 // @description  98堂色花堂磁链直达
-// @author       93xo.ox39@gmail.com
+// @author       ifwlzs
 // @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
-    document.querySelector(".blockcode > div > ol > li").innerHTML="<a href=\'"+document.querySelector(".blockcode > div > ol > li").textContent+" \'>"+document.querySelector(".blockcode > div > ol > li").textContent+"</a>"
+    var magnet=document.querySelector(".blockcode > div > ol > li").textContent
+    document.querySelector(".blockcode > div > ol > li").innerHTML="<a href=\'"+magnet+" \'>"+magnet+"</a>"
     // Your code here...
 })();
