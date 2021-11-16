8780c120a91a889b258dd539ad512487c0cfdaf6
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-11-16 16:20:22 +0800

    Create 恋爱游戏网签到.js

---
+++ 恋爱游戏网签到.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         恋爱游戏网签到
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       ifwlzs
+// @match        https://www.lianaiyx.com/e/member/cp/
+// @icon         https://www.lianaiyx.com/favicon.ico
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     document.querySelector("#sign").click()
+    // Your code here...
+})();
