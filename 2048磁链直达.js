f4081fab05728922e83439e84a376b52e935c2e3
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-11-16 16:12:08 +0800

    Update 2048磁链直达.js

--- 2048磁链直达.js
+++ 2048磁链直达.js
@@ -3,12 +3,17 @@
 // @include      *://*.*.*/2048/*
 // @version      0.1
 // @description  2048磁链直达
-// @author       93xo.ox39@gmail.com
+// @author       ifwlzs
 // @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
-document.querySelector("#code1 > ol > li").innerHTML="<a href=\'"+document.querySelector("#code1 > ol > li").textContent+" \'>"+document.querySelector("#code1 > ol > li").textContent+"</a>"
+    var magnet=document.getElementById("code2").textContent;
+    document.getElementById("code2").innerHTML="<a href=\""+magnet+"\">"+magnet+"</a>"
+    var magnet1=document.querySelector("#code1 > ol > li").textContent;
+    document.querySelector("#code1 > ol > li").innerHTML="<a href=\'"+magnet1+" \'>"+magnet1+"</a>"
     // Your code here...
+
 })();
