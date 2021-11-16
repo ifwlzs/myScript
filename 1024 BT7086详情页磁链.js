e753a040fe1d08be65adf0eb97daf3117d3a9d89
ifwlzs <49548316+ifwlzs@users.noreply.github.com>
2021-11-16 16:23:33 +0800

    Update 1024 BT7086详情页磁链.js

--- 1024 BT7086详情页磁链.js
+++ 1024 BT7086详情页磁链.js
@@ -1,14 +1,18 @@
 // ==UserScript==
 // @name         1024 BT7086详情页磁链
 // @include      *://*.downsx.*/*
-// @version      0.1
+// @include      *://*.*.*/torrent/*
+// @include      *://bitsdts.*/*
+// @version      0.2
 // @description  1024 BT7086详情页磁链
-// @author       93xo.ox39@gmail.com
+// @author       ifwlzs
 // @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
-document.querySelector('.dlboxbg').innerHTML="<a href=\'"+document.querySelectorAll(".uk-button")[1].href+" \'>"+document.querySelectorAll(".uk-button")[1].href+"</a>"
+    var url=document.querySelectorAll(".uk-button")[1].href
+    document.querySelector('.dlboxbg').innerHTML="<a href=\'"+url+" \'>"+url+"</a>"
     // Your code here...
 })();
