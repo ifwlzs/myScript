53c08c143d4c9adc1148916b02709295d6325bc4
ifwlzs <93xo.ox39@gmail.com>
2021-11-16 17:11:01 +0800

    Update 2048磁链直达.js

--- 2048磁链直达.js
+++ 2048磁链直达.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         2048磁链直达
 // @include      *://*.*.*/2048/*
-// @version      0.2
+// @version      0.3
 // @description  2048磁链直达
 // @author       ifwlzs
 // @grant        none
@@ -11,8 +11,8 @@
 
 (function() {
     'use strict';
-    var magnet=document.getElementById("code2").textContent;
-    document.getElementById("code2").innerHTML="<a href=\""+magnet+"\">"+magnet+"</a>"
+    var magnet=document.getElementsByClassName("blockquote2")[0].textContent;
+    document.getElementsByClassName("blockquote2")[0].innerHTML="<a href=\""+magnet+"\">"+magnet+"</a>"
     var magnet1=document.querySelector("#code1 > ol > li").textContent;
     document.querySelector("#code1 > ol > li").innerHTML="<a href=\'"+magnet1+" \'>"+magnet1+"</a>"
     // Your code here...
